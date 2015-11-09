1df87973ed50862f39211280b2edda275f474994
demiurgosoft <demiurgosoft@hotmail.com>
2015-11-09 13:01:46 +0100

    brujeria con node

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# brujeria-con-node
-Charla-Taller de Node.js con implementación de un servicio de chat
+Brujeria con Node.js
+====================
+_by @demiurgosoft_
+Charla-Taller de Node.js con implementación de un servicio de chat _MyChat_
+
+Todo el código se encuentra bajo una **Licencia MIT**
+Charla y otras cosas bajo **Licencia CC-by-sa**
---
+++ mychat/server.js
@@ -0,0 +1,34 @@
+var express = require("express"); //"loads" express framework library
+var app = express(); //new express app (some magic here)
+var http = require('http').Server(app); //http server using app
+var io = require('socket.io')(http); //attach out socket.io to our server
+
+
+app.use(express.static('public'));
+
+//what happens with a get request to our server?
+app.get('/', function(req, res) {
+	//this function is a magical callback
+	//req is the request info, response is used to send our funny stuff
+	res.redirect('/client.html');
+});
+
+//when a socket connects
+io.on('connection', function (socket) { //the callback arg is the connected socket
+ 	console.log("User connected");
+
+	//attach an event listener (msg) to connected socket
+	socket.on('msg', function (content) {
+		console.log("Received:"+content);
+  	});
+	//will fire when socket disconnects
+	socket.on('disconnect', function () {
+		io.emit('User disconnected');
+	});
+});
+
+//server starts listening to port 9090
+http.listen(9090, function() {
+	//Another magical callback
+	console.log("Magic happens in port 9090");
+});
